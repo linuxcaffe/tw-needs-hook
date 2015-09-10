70eb1b3d46cb035d79f7a7d7cf1d1dde411aadd6
djp <davamundo@gmail.com>
2015-09-10 17:32:00 -0400

    added check_needs.sh

---
+++ check_needs.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+# check_needs.sh
+# Copyright (C) 2015 djp <djp@transit>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+# This script is intended for use in assigning a need:value for all +PENDING tasks.
+# when it is run, it assigns a default value of "4", and provides instructions
+# on how to assign need:value to tasks with specific projects or tags.  
