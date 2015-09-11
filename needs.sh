dc68c5381d21cf558c4756ae536a085ac4b8598e
David J Patrick <davamundo@gmail.com>
2015-09-11 18:25:16 -0400

    Update needs.sh

--- needs.sh
+++ needs.sh
@@ -64,42 +64,5 @@ exit 0
 
 NEED_LEV=$1
 CONJUNCTION=' and '
+exit 0
 
-# TODO: fix everything below!! all fscked up!
-
-# find current context, 
-WAS_CONTEXT=`task _get rc.context`
-
-# save if any
-
-# define new context
-
-# apply new context
-
-# reset previous context
-
-
-  if [[ $WAS_CONTEXT == '' ]]
-    then
-      CONJUNCTION=''
-      IS_CONTEXT=N$1
-      echo "No context was previously set. Context is now $IS_CONTEXT"
-
-  elif [[ $WAS_CONTEXT == $OLD_CONTEXT ]]
-    then
-      echo "No change in context"
-  fi
-  if [[ $WAS_CONTEXT != '' ]]
-    then
-      echo "Context was $WAS_CONTEXT"
-      task confirmation=off config was.context $WAS_CONTEXT
-      echo "Setting needs level to $NEED_LEV"
-      WAS_CONTEXT_FILTER=`task _get rc.context.$WAS_CONTEXT`
-      echo "filter was $WAS_CONTEXT_FILTER"
-      IS_CONTEXT="$WAS_CONTEXT .N$1"
-      task confirmation=off config context $IS_CONTEXT
-      N_CONTEXT="tw '! ((need.over:$NEED_LEV or need.under:1) and ! (due:today or until:tomorrow))'"
-      IS_CONTEXT_FILTER=$WAS_CONTEXT_FILTER$CONJUNCTION$N_CONTEXT
-      task confirmation=off config context.$1 $IS_CONTEXT_FILTER
-      exit 0
-  fi
