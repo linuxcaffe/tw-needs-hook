d6b2107a579e4f86879a9d6d6ff706783651a19d
djp <davamundo@gmail.com>
2015-09-15 18:04:05 -0400

    on-exit_needs.sh not working

--- on-exit_needs.sh
+++ on-exit_needs.sh
@@ -10,9 +10,10 @@
 # - Optional feedback/error.
 
 TASK_BIN=/usr/bin/task
-NEED_LEV=$($TASK_BIN _get rc.needlevel)
-if [[ $NEED_LEVEL -gt "0" ]]; then
+NEED_LEV=`task _get rc.needlevel`
+if [[ "$NEED_LEVEL" =~ "[1-6]+" ]]; then
 echo "Need-level filterring set to $NEED_LEV or lower"
+exit 0
 fi
 
 # Status:
