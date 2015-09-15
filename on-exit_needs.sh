adf4fe1cffbca5a6e37487e37f01abd37d33dfd8
djp <davamundo@gmail.com>
2015-09-15 17:44:30 -0400

    added on-exit script for message

--- on-exit_needs.sh
+++ on-exit_needs.sh
@@ -1,13 +1,21 @@
-#! /bin/sh
-#
-# on-exit_needs.sh
-# Copyright (C) 2015 djp <djp@transit>
-#
-# Distributed under terms of the MIT license.
-#
+#!/bin/bash
 
-# This is the hook part of tw-needs-hook
+# The on-exit event is triggered once, after all processing is complete.
+# This hook script has no effect on processing.
 
-need_level=`task _get rc.needs`
+# Input:
+# - line of JSON for each modified task
 
+# Output:
+# - Optional feedback/error.
 
+TASK_BIN=/usr/bin/task
+NEED_LEV=$($TASK_BIN _get rc.needlevel)
+if [[ $NEED_LEVEL -gt "0" ]]; then
+echo "Need-level filterring set to $NEED_LEV or lower"
+fi
+
+# Status:
+# - 0:     Non-JSON is feedback.
+# - non-0: Non-JSON is error.
+exit 0
